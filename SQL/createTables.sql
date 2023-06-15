1a42b56ce2c4bd7eb10e5f33fe6704f748b2570a
liyanse <lianswanjiku@icloud.com>
2023-06-15 16:45:04 +0300

    insert values

--- SQL/createTables.sql
+++ SQL/createTables.sql
@@ -1,8 +1,8 @@
 ## Creating table and adding respective columns
 CREATE TABLE employee(
     employee_id INT PRIMARY KEY,
-    name VARCHAR(20),
-    department VARCHAR(20)
+    name VARCHAR(20) NOT NULL,
+    department VARCHAR(20) UNIQUE
 );
 
 ## Describe to see if table structure is similar to what we want
@@ -12,4 +12,4 @@ DESCRIBE employee;
  DROP TABLE employee;
 
 ## alter table by either adding new columns or dropping columns
- ALTER TABLE employee add salary DECIMAL;
+ ALTER TABLE employee add salary DECIMAL NOT NULL;
---
+++ SQL/insertValues.sql
@@ -0,0 +1,13 @@
+##Add the first columns into our table
+INSERT INTO employee VALUES(1, 'Jack', 'FrontEnd', '10000');
+
+## retrieve all the information from the table
+select * from employee
+
+
+INSERT INTO employee (employee_id, name, department, salary) VALUES
+(2, 'Kate','Marketing','4000'),
+(3,'Claire','AI','5000'),
+(4,'Jack','HR','8000')
+;
+
