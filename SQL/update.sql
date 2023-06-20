f0a11585796a767b3fe49d8ba7ca8befe6344c42
liyanse <lianswanjiku@icloud.com>
2023-06-20 14:43:57 +0300

    Update update.sql

--- SQL/update.sql
+++ SQL/update.sql
@@ -12,3 +12,13 @@ WHERE sale_id = 1007
 #rename table
 ALTER refinery_table
 RENAME TO ref_table;
+
+## add a new COLUMN
+ALTER TABLE dis_channels
+ADD location TEXT NULL;
+
+##add values to a newly added column
+UPDATE dis_channels 
+SET location = 'County A'
+WHERE sale_id =1001 OR sale_id = 1003
+OR sale_id = 1006 OR sale_id = 1004;
