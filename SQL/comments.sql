215d5e9a090c3f048e6240dff059ea59875760aa
liyanse <lianswanjiku@icloud.com>
2023-06-20 15:05:05 +0300

    comments

---
+++ SQL/comments.sql
@@ -0,0 +1,9 @@
+-- Hey Sarah Please update any status_received values
+
+/*
+Hey Srah please plugin any updated values on the status_received column
+Thanks so much for your hard work
+Lians :)
+*/
+
+SELECT dis_name,status_received FROM dis_channels;
