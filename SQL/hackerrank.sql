2b92f54798198f33e8a6d94878fff0208370a50b
liyanse <lianswanjiku@icloud.com>
2023-06-20 17:57:54 +0300

    Update hackerrank.sql

--- SQL/hackerrank.sql
+++ SQL/hackerrank.sql
@@ -28,3 +28,19 @@ WHEN grd.grade >= 8 THEN std.name END,
 grd.grade, std.marks FROM students std, grades grd
 WHERE std.marks BETWEEN grd.min_mark AND grd.max_mark
 ORDER BY grd.grade DESC, std.name ASC;
+
+/*
+Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
+Write a query to print the respective hacker_id and name of hackers who achieved full scores 
+for more than one challenge. Order your output in descending order by the total number of challenges 
+in which the hacker earned a full score. 
+If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+*/
+select h.hacker_id, h.name from Submissions as s join Hackers as h 
+on s.hacker_id = h.hacker_id 
+join Challenges as c on s.challenge_id = c.challenge_id
+join Difficulty as d on c.Difficulty_level = d.Difficulty_level
+where s.score = d.score 
+group by h.hacker_id, h.name 
+having count(*) > 1
+order by count(*) desc, h.hacker_id;
