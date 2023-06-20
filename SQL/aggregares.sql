692cffc7a377ec0149d3bead7a834d476ef4e331
liyanse <lianswanjiku@icloud.com>
2023-06-20 14:47:44 +0300

    Update aggregares.sql

--- SQL/aggregares.sql
+++ SQL/aggregares.sql
@@ -1,4 +1,5 @@
-### An aggregate function in SQL performs a calculation on multiple values and returns a single value. SQL provides many aggregate functions that include avg, count, sum, min, max, etc. An aggregate function ignores NULL values when it performs the calculation, except for the count function. 
+### An aggregate function in SQL performs a calculation on multiple values and returns a single value. 
+##SQL provides many aggregate functions that include avg, count, sum, min, max, etc. An aggregate function ignores NULL values when it performs the calculation, except for the count function. 
 
 ##avg
 SELECT AVG (salary)
