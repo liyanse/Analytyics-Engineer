bd20b521dfb966497144f548d003f35db26cb2ac
liyanse <lianswanjiku@icloud.com>
2023-06-20 17:53:32 +0300

    Update hackerrank.sql

--- SQL/hackerrank.sql
+++ SQL/hackerrank.sql
@@ -8,3 +8,23 @@ SELECT Name
 FROM STUDENTS
 WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID ASC;
+
+
+/*
+Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. 
+Ketty doesn't want the NAMES of those students who received a grade lower than 8. 
+The report must be in descending order by grade -- i.e. higher grades are entered first. 
+If there is more than one student with the same grade (8-10) assigned to them, 
+order those particular students by their name alphabetically.
+Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order.
+If there is more than one student with the same grade (1-7) assigned to them, 
+order those particular students by their marks in ascending order.
+
+Write a query to help Eve.
+*/
+SELECT 
+CASE WHEN grd.grade < 8 THEN NULL 
+WHEN grd.grade >= 8 THEN std.name END,
+grd.grade, std.marks FROM students std, grades grd
+WHERE std.marks BETWEEN grd.min_mark AND grd.max_mark
+ORDER BY grd.grade DESC, std.name ASC;
