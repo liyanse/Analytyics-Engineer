0adf70affeb90bef8d3cdba4ef649e5cc8aa9bd2
liyanse <lianswanjiku@icloud.com>
2023-06-22 15:35:27 +0300

    Update case.sql

--- SQL/case.sql
+++ SQL/case.sql
@@ -7,3 +7,14 @@ END
 FROM refinery_clients
 WHERE status_sent IS NOT NULL
 ORDER BY sale_id;
+
+
+SELECT dis_name, status_received, location,
+CASE
+	WHEN status_received = 'false' THEN 'Order Incomplete'
+	WHEN status_received = 'true' THEN 'Complete Order'
+	ELSE 'Unknown'
+END
+FROM dis_channels
+JOIN refinery_clients
+ON dis_channels.sale_id = refinery_clients.sale_id
