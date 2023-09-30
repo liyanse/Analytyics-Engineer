30f58e282520bf07176cf3aac1b79c267b12c3cb
Lians <91662935+liyanse@users.noreply.github.com>
2023-09-30 12:08:32 +0300

    1223

--- lux_class2/forloop.py
+++ lux_class2/forloop.py
@@ -1,19 +1,23 @@
-print("1st example")
-
-lst = [1, 2, 3]
-for i in range(len(lst)):
-     print(lst[i], end = " \n")
- 
-print("2nd example")
-
-for j in range(0,5):
-    print(j, end = " \n")
-
-
-
 """
 For loop Question: 
 Write a Python program that iterates the integers from 1 to 50.
  For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
  For numbers that are multiples of three and five, print "FizzBuzz".
  """
+##Iterate 1 -50 (51) done
+## Fizz for multiples on 3 instead of the number
+## Buzz for multiples on 5 instead of the number
+## FizzBuzz for multiples on 3 & 5 instead of the number
+
+for fizzbuzz in range(1,51): 
+     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+          print("fizzbuzz")
+     elif fizzbuzz % 3 == 0:
+          print("fizz")
+     else:
+          print("buzz")
+     print(fizzbuzz)
+
+     
+
+
--- lux_class2/whileloop.py
+++ lux_class2/whileloop.py
@@ -1,14 +1,32 @@
-m = 5
-i = 0
-while i < m:
-     print(i, end = " ")
-     i = i + 1
-print("End")
+import random
 
+while True:
+    player_action = input("Rock paper scissors: ")
+    possible_action = ['rock', 'paper', 'scissors']
+    computer = random.choice(possible_action)
+    print(f"\nYou chose {player_action}, computer chose {computer}.\n")
 
-"""
+    if player_action == computer:
+        print(f"You both chose {player_action}. It's a draw!")
 
-While loop Question: 
-Rock Paper Scissors
+    elif player_action == "rock":
+        if computer == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
 
- """
+    elif player_action == "paper":
+        if computer == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    
+    elif player_action == "scissors":
+        if computer == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+        
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
